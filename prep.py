f6d86e37ef516f6fbd3cf66553fb576b4bc5381e
sudhirsingaraju79-oss <sudhirsingaraju79@gmail.com>
2025-12-07 12:06:58 +0530

    Add files via upload

--- prep.py
+++ prep.py
@@ -13,7 +13,8 @@ from huggingface_hub import login, HfApi
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
-DATASET_PATH = "hf://datasets/sudhirpgcmma02/visit-us-prediction/tourism.csv"
+#DATASET_PATH = "hf://datasets/sudhirpgcmma02/visit-us-prediction/tourism.csv"
+DATASET_PATH = "hf://datasets/tourism.csv"
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
